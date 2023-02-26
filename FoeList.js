b26ae3ed91fd545cb8fe5cc648e2378ba4f8ec97
Victor Bussiki <victorbussiki@gmail.com>
2023-02-26 11:21:41 -0300

    Refactored code to properly differentiate foes

--- FoeList.js
+++ FoeList.js
@@ -4,14 +4,14 @@ class FoeList {
     #numberOfMonsters;
     #hdScore;
     #hdModifier;
-    #hdArray;
+    #foeList;
 
     constructor(foeName, numberOfMonsters, hdScore, hdModifier) {
         this.#foeName = foeName;
         this.#numberOfMonsters = numberOfMonsters;
         this.#hdScore = hdScore;
         this.#hdModifier = hdModifier;
-        this.#hdArray = this.#hdRoller(numberOfMonsters, hdScore, hdModifier)
+        this.#foeList = this.#hdRoller(numberOfMonsters, hdScore, hdModifier)
     } 
 
     #rollDice(numberOfDice, numberOfSides) {
@@ -22,44 +22,48 @@ class FoeList {
     }
 
     #hdRoller(numberOfMonsters, hdNumber, modifier) {
-        let hdArray = [];
+        let foeList = [];
         let roll = 0;
         for(let i = 0; i < numberOfMonsters; i++) {
             roll = this.#rollDice(hdNumber, 8);
-            let rollResult = roll + modifier;
-            hdArray.push(rollResult);
+            let foe = {
+                "name" : `${this.#foeName} ${i + 1}`,
+                "hitPoints" : roll + modifier
+            }
+            foeList.push(foe);
         }
-        return hdArray;
+        return foeList;
     }
 
     status() {
-        let numberOfLivingMonsters = this.#hdArray.length;
+        let numberOfLivingMonsters = this.#foeList.length;
 
         if (numberOfLivingMonsters === 0) {
             console.log("All foes are vanquished!")
         } else {
-            console.log("There are " + numberOfLivingMonsters + " living " + this.#foeName + "s...");
-            console.log("Their current HP is: ")
-            
+            console.log(`There are ${numberOfLivingMonsters} living ${this.#foeName}s`);
+            console.log("Their current HP is: ");
+
             for(let i = 0; i < numberOfLivingMonsters; i++) {
-                let monsterStatus = `${this.#foeName} ${i + 1} : ${this.#hdArray[i]} hit points`
+                let monsterStatus = `${this.#foeList[i].name} : ${this.#foeList[i].hitPoints} hit points`
                 console.log(monsterStatus);
             }
         }
     }
 
     damage(creatureIndex, damage) {
-        this.#hdArray[creatureIndex] -= damage;
-        let damageMessage = `${this.#foeName} ${creatureIndex + 1} lost ${damage} hit points!`
+        this.#foeList[creatureIndex].hitPoints -= damage;
+        let damageMessage = `${this.#foeList[creatureIndex].name} lost ${damage} hit points!`
         console.log(damageMessage);
         
-        if(this.#hdArray[creatureIndex] <= 0) {
-            this.#hdArray.splice(creatureIndex, 1);
-            let deathMessage = `${this.#foeName} ${creatureIndex + 1} has been killed!`
+        if(this.#foeList[creatureIndex].hitPoints <= 0) {
+            let deathMessage = `${this.#foeList[creatureIndex].name} has been killed!`;
+            this.#foeList.splice(creatureIndex, 1);
             console.log(deathMessage);
         }
         this.status();
     }
+
 }
 
 module.exports = FoeList; 
--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const FoeList = require('./FoeList');
 
 console.log('Running Enemy Tracker...');
 
+// Test foe list
+
+let gnolls = new FoeList("Gnoll", 4, 2, 0);
+
