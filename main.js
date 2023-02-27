03d6bbabdf99b1934ae38c7286fa9c70cafc6a34
Victor Bussiki <victorbussiki@gmail.com>
2023-02-27 17:28:52 -0300

    Improved spacing and terminal readibility

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const FoeList = require('./FoeList'); 
 
 console.log('Running Enemy Tracker...');
+console.log("");
 
-// Test foe list
+// Foe list test:
 
 let gnolls = new FoeList("Gnoll", 4, 2, 0);
-
+gnolls.status();
